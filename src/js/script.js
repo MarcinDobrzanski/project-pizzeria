0b0d6bfad8178fb5d7a96d0fe51b1e87c0ef3009
Marcin <marcin@dobrzanski.me>
2022-12-07 23:40:18 +0100

    Add listener remove

--- src/js/script.js
+++ src/js/script.js
@@ -529,10 +529,21 @@
       });
 
       thisCartProduct.dom.wrapper.dispatchEvent(event);
+      console.log('remove', thisCartProduct);
     }
 
     initActions() {
-      
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function (event) {
+        event.preventDefault();
+
+      });
+
+      thisCartProduct.dom.remove.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisCartProduct.remove();
+      });
     }
   }
 

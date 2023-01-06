62d3211f487f69dff6dd3aefef96f4442be1512c
Marcin <marcin@dobrzanski.me>
2023-01-06 13:25:19 +0100

    Remove consol log from bannerClick

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -27,13 +27,12 @@ class Home {
     const thisHome = this;
 
     thisHome.dom.banner = document.querySelectorAll(select.banner.links);
-    console.log('thisHome.dom.banner', thisHome.dom.banner);
     thisHome.pages = document.querySelectorAll(select.containerOf.pages);
     thisHome.bannerWrapper = document.querySelector(select.banner.bannerWrapper);
     thisHome.activePage = document.querySelector(select.containerOf.pages).children;
     thisHome.navLinks = document.querySelectorAll(select.nav.links);
 
-    console.log('thisHome.activePage', thisHome.activePage);
+
     thisHome.bannerWrapper.addEventListener('click', function (event) {
 
       const idFromHash = event.target.offsetParent.children[0].hash.replace('#/', '');
@@ -42,10 +41,7 @@ class Home {
       if (event.target.classList.contains(select.banner.bannerOrder)) {
 
         for (let page of thisHome.activePage) {
-          console.log('page', page);
           page.classList.remove(classNames.pages.active);
-          console.log('page.id', page.id);
-          console.log('idFromHash', idFromHash);
 
           if (page.id == idFromHash) {
 
@@ -56,8 +52,6 @@ class Home {
         for (let link of thisHome.navLinks) {
           link.classList.remove(classNames.nav.active);
           const idLinks = link.getAttribute('href');
-          console.log('idHash', idHash);
-          console.log('idLinks', idLinks);
           if (idLinks == idHash) {
             link.classList.add(classNames.nav.active);
           }
